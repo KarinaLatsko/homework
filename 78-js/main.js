1d849d63ce55d883794199781109a7f9b9763fbd
KarinaLatsko <160108534+KarinaLatsko@users.noreply.github.com>
2024-04-17 00:09:16 +0700

    add lesson 78

---
+++ 78-js/main.js
@@ -0,0 +1,21 @@
+const instrument = 'Инструмент'
+const box = 'ящик'
+console.log(`${instrument} лежит в ${box}е`)
+
+// let instrument = 'Инструмент'
+// instrument = 10
+// console.log(instrument)
+
+const user = 'User'
+const age = 25
+const isAdmin = true
+console.log(`${user} admin: ${isAdmin}`)
+
+console.log(typeof(isAdmin))
+
+let foo
+console.log(foo)
+
+let sum = null
+sum = 5 + 5
+console.log(sum) 
