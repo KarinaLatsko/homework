b72c0b9c15f7566656dc3f509b4aac2e81f7722b
KarinaLatsko <160108534+KarinaLatsko@users.noreply.github.com>
2024-04-17 00:05:01 +0700

    add lesson 76

---
+++ 76-js-let-const/main.js
@@ -0,0 +1,20 @@
+// const firstName = 'John'
+// const lastName = 'Smith'
+// let age = 20
+
+
+
+
+// console.log(firstName, lastName, age, 'y.o.')
+
+
+
+const firstName = 'Karina'
+const lastName = 'Lacko'
+const myBirthday = '06.06.1986'
+let age = '37'
+let maritalStatus = 'married'
+
+console.log(firstName, lastName)
+console.log(myBirthday, age, 'years old')
+console.log(maritalStatus)
