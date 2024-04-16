d3ef04b0511fa2b9b80985aa50d22069607b489f
KarinaLatsko <160108534+KarinaLatsko@users.noreply.github.com>
2024-04-17 00:07:44 +0700

    Add lesson 77

---
+++ 77-js-operators-operands/main.js
@@ -0,0 +1,38 @@
+// const sum = 2 + 2
+// console.log(sum)
+
+// const minus = 10 - 5
+// console.log(minus)
+
+// const x = 10
+// const y = 20
+// const result = x * y
+// console.log(result)
+
+// const x = 30
+// const y = 20
+// console.log(x * y)
+
+// const x = 10
+// const y = 20
+// const devision = y / 2
+// console.log(devision)
+
+// console.log(3**2)
+
+// console.log(5 % 2)
+
+// console.log('Hello' + 'World')
+
+// const num = 2
+// console.log(-num)
+
+const sum = 6 + 6
+console.log(sum)
+
+const x = 5
+const y = 10
+console.log(x * y)
+
+console.log('День ' + 'космонавтики')
+
