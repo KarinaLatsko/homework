5513c15ac83c50941259272ac9034ca3e1c65231
KarinaLatsko <160108534+KarinaLatsko@users.noreply.github.com>
2024-04-18 23:43:06 +0700

    add lesson 80

---
+++ 80-js/main.js
@@ -0,0 +1,35 @@
+// let i = 0
+// while(i < 10) {
+//   i = i + 1
+//   console.log(i)
+// }
+
+// let i = 0
+// do {
+//   i++
+//   console.log(i)
+// } while (i<5)
+
+// for(let i = 1; i <= 10; i++) {
+//   console.log(`Пройден ${i} круг`)
+// }
+
+for(let i = 2; i <= 12; i = i + 2) {
+  console.log(i)
+}
+
+for(let i = 10; i > 0; i = i - 1) {
+  console.log(i)
+}
+
+let i = 0
+while(i < 12) {
+  i = i + 2
+  console.log(i)
+}
+
+// let i = 11
+// while(i > 1) {
+//   i = i - 1
+//   console.log(i)
+// }
