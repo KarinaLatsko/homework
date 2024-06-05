141073ed5b23bff9012c569e0ff9f396dcb57463
KarinaLatsko <160108534+KarinaLatsko@users.noreply.github.com>
2024-06-06 01:23:21 +0700

    Add lesson 117

--- wake-bake-homework/js/main.js
+++ wake-bake-homework/js/main.js
@@ -71,15 +71,49 @@
     const controlActive = document.querySelector('.tab-controls__link--active')
 
     if (controlActive)
-    controlActive.classList.remove('tab-controls__link--active')
+      controlActive.classList.remove('tab-controls__link--active')
 
     if (contentActive)
-    contentActive.classList.remove('tab-content--show')
-    
+      contentActive.classList.remove('tab-content--show')
+
     tabControl.classList.add('tab-controls__link--active')
     tabContent.classList.add('tab-content--show')
-    
   }
 
-})()
 
+  // Аккордеон
+
+  const accordionlists = document.querySelectorAll('.accordion-list')
+
+  accordionlists.forEach(el => {
+
+    el.addEventListener('click', (e) => {
+
+      const accordionList = e.currentTarget
+      const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+      const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+      const accordionControl = e.target.closest('.accordion-list__control');
+
+      if (!accordionControl) return
+
+      const accordionItem = accordionControl.parentElement;
+      const accordionContent = accordionControl.nextElementSibling;
+
+      if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+        accordionOpenedItem.classList.remove('accordion-list__item--opened');
+        accordionOpenedContent.style.maxHeight = null;
+      }
+
+      accordionItem.classList.toggle('accordion-list__item--opened');
+
+      if (accordionItem.classList.contains('accordion-list__item--opened')) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+      } else {
+        accordionContent.style.maxHeight = null;
+      }
+
+
+    })
+  })
+})()
