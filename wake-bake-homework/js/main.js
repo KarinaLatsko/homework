2bf8b3727273c13a6616e9b54b27f4f167cba8bb
KarinaLatsko <160108534+KarinaLatsko@users.noreply.github.com>
2024-06-20 00:53:18 +0700

    Add lesson 122

--- wake-bake-homework/js/main.js
+++ wake-bake-homework/js/main.js
@@ -113,7 +113,71 @@
         accordionContent.style.maxHeight = null;
       }
 
-
     })
+  })
+
+
+  // Слайдер-галерея
+
+  const swiper = new Swiper('.gallery__slider', {
+    
+    spaceBetween: 15,
+    slidesPerView: 1.5,
+
+    pagination: {
+      el: '.gallery__pagination',
+      type: 'fraction',
+    },
+
+    navigation: {
+      nextEl: '.gallery__next',
+      prevEl: '.gallery__prev',
+    },
+
+    breakpoints: {
+      
+      451: {
+        slidesPerView: 2,
+      },
+      601: {
+        slidesPerView: 3,
+      },
+      801: {
+        spaceBetween: 32,
+      },
+      1101: {
+        slidesPerView: 4,
+      }
+    }
+
+  })
+
+  // Слайдер-отзывы
+
+  new Swiper('.testimonials__slider', {
+    
+    spaceBetween: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+
+    navigation: {
+      nextEl: '.testimonials__next',
+      prevEl: '.testimonials__prev',
+    },
+
+    scrollbar: {
+      el: '.testimonials__scrollbar',
+      draggable: true,
+    },
+
+    breakpoints: {
+      901: {
+        slidesPerView: 1.5,
+      },
+      1201: {
+        slidesPerView: 2.05,
+      }
+    }
+
   })
 })()
