eb94d98dad65fd1e34f50911baac1be0a85e3fd2
KarinaLatsko <160108534+KarinaLatsko@users.noreply.github.com>
2024-06-23 23:35:27 +0700

    add lesson 124

--- wake-bake-homework/js/main.js
+++ wake-bake-homework/js/main.js
@@ -180,4 +180,10 @@
     }
 
   })
+
+  // Маска телефона
+
+  const telInput = document.querySelectorAll('input[type="tel"]')
+  const im = new Inputmask('+7 (999) 999-99-99')
+  im.mask(telInput)
 })()
