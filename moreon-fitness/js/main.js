5fa1db8702d206161a923e97d06ebc5e881af662
KarinaLatsko <160108534+KarinaLatsko@users.noreply.github.com>
2024-07-06 02:02:25 +0700

    Add graduation project

--- moreon-fitness/js/main.js
+++ moreon-fitness/js/main.js
@@ -1,25 +1,48 @@
 (function () {
 
-  // Модалки
+  // Модалка "Узнать подробнее"
 
-  const modal = document.querySelector('.modal')
-  const modalButton = document.querySelector('.header__button-about')
+  const modal = document.querySelector('.modal-about')
+  const modalButton = document.querySelector('.hero__button-about')
 
-  modalButton.addEventListener('click', openModal)
-  modal.addEventListener('click', closeModal)
+  modalButton.addEventListener('click', openModalAbout)
+  modal.addEventListener('click', closeModalAbout)
 
-  function openModal(e) {
+  function openModalAbout(e) {
     e.preventDefault()
-    document.body.classList.toggle('body--opened-modal')
+    document.body.classList.toggle('body--opened-modal-about')
   }
 
-  function closeModal(e) {
-    e.preventDefault()
+  function closeModalAbout(e) {
+    // e.preventDefault()
 
     const target = e.target
 
-    if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
-      document.body.classList.remove('body--opened-modal')
+    if (target.closest('.modal-about__cancel') || target.classList.contains('modal-about')) {
+      document.body.classList.remove('body--opened-modal-about')
+    }
+  }
+
+  // Модалка "Рассчитать стоимость"
+
+  const modalCalc = document.querySelector('.modal-calculator')
+  const modalButtonCalc = document.querySelector('.hero__button-price')
+
+  modalButtonCalc.addEventListener('click', openModalCalc)
+  modalCalc.addEventListener('click', closeModalCalc)
+
+  function openModalCalc(e) {
+    e.preventDefault()
+    document.body.classList.toggle('body--opened-modal-calc')
+  }
+
+  function closeModalCalc(e) {
+    // e.preventDefault()
+
+    const target = e.target
+
+    if (target.closest('.modal-calculator__cancel') || target.classList.contains('modal-calculator')) {
+      document.body.classList.remove('body--opened-modal-calc')
     }
   }
 
